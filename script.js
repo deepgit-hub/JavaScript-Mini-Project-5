bdb22b3f58587192c7ef4c57ea7d16aa66a337f9
Deepak <darkdeepak001@gmail.com>
2026-06-11 17:47:19 +0530

    Initialize variables and get DOM elements in script.js

--- script.js
+++ script.js
@@ -1,1 +1,12 @@
-
+var ni = document.getElementById("name-input")
+var mi = document.getElementById("mark-input")
+var dt = document.getElementById("data-table")
+var rt = document.getElementById("report-table")
+var studentcount = 0
+var passedstudents =0
+var failedstudents =0
+var nestudents=0
+var sww = 0
+var highm = 0
+var lowm = 100
+var avgm =0
