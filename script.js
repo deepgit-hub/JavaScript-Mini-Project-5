d8ec0a867c277bd753df0f97c54a9fc3b72676c6
Deepak <darkdeepak001@gmail.com>
2026-06-11 18:49:29 +0530

    Refactor student input handling in script.js

--- script.js
+++ script.js
@@ -1,5 +1,9 @@
 var ni = document.getElementById("name-input")
-var mi = document.getElementById("mark-input")
+var mo = document.getElementById("mark1-input")
+var mt = document.getElementById("mark2-input")
+var mth = document.getElementById("mark3-input")
+var mf = document.getElementById("mark4-input")
+var mfv = document.getElementById("mark5-input")
 var di = document.getElementById("department-input")
 var yi = document.getElementById("year-input")
 var dt = document.getElementById("data-table")
@@ -14,40 +18,39 @@ var lowm = 100
 var avgm =0
 var total_mark=0
 var am = 0
+var result=""
+var p = 0
 function addi()
 {
-    if(ni.value=="" || mi.value=="" || di.value=="" || yi.value=="")
+    if(ni.value=="" || mo.value=="" || mt.value=="" || mth.value==""|| mf.value=="" || mfv.value=="" || di.value=="" || yi.value=="")
     {
         alert("Please fill all the feilds")
         return
     }
-    var l = document.createElement("tr")
-    l.innerHTML ="<td>"+ni.value+"</td>"+"<td>"+mi.value+"</td>"+"<td>"+di.value+"</td>"+"<td>"+yi.value+"</td>"
-    dt.append(l)
-    
-
     studentcount++
-    if(Number(mi.value)>=40)
+
+    total_mark = Number(mo.value)+Number(mt.value)+Number(mth.value)+Number(mf.value)+Number(mfv.value)
+    p = (total_mark*5)/100
+    if(p>75)
     {
-        passedstudents++
+        result="Pass"
     }
     else{
-        failedstudents++
+        result ="Fail"
     }
-    if(Number(mi.value)>highm)
-    {
-        highm=Number(mi.value)
-    }
-    if(Number(mi.value)<lowm)
-    {
-        lowm=Number(mi.value)
-    }
-    total_mark+=Number(mi.value)
-    avgm = total_mark/studentcount
+    var l = document.createElement("tr")
+    l.innerHTML ="<td>"+ni.value+"</td>"+"<td>"+di.value+"</td>"+"<td>"+yi.value+"</td>"+"<td>"+p+"</td>"+"<td>"+result+"</td>"
+    dt.append(l)
+    
     ni.value=""
-    mi.value=""
+    mo.value=""
+    mt.value=""
+    mth.value=""
+    mf.value=""
+    mfv.value=""
     di.value=""
     yi.value=""
+    
 }
 function report(event)
 {
