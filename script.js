f26080653179434df5d3e0484d5cbdbf28be9a8a
Deepak <darkdeepak001@gmail.com>
2026-06-11 17:47:48 +0530

    Add function to handle student data and metrics

--- script.js
+++ script.js
@@ -10,3 +10,34 @@ var sww = 0
 var highm = 0
 var lowm = 100
 var avgm =0
+function addi()
+{
+    var l = document.createElement("tr")
+    l.innerHTML ="<td>"+ni.value+"</td>"+"<td>"+mi.value+"</td>"+"<td>"+ai.value+"</td>"+"<td>"+li.value+"</td>"+"<td>"+ml.value+"</td>"
+    dt.append(l)
+    studentcount++
+    if(Number(mi.value>=40))
+    {
+        passedstudents++
+    }
+    else{
+        failedstudents++
+    }
+    if(Number(ai.value<75))
+    {
+        nestudents++
+    }
+    else
+    {
+        sww++
+    }
+    if(Number(mi.value)>highm)
+    {
+        highm=Number(mi.value)
+    }
+    if(Number(mi.value)<lowm)
+    {
+        lowm=Number(mi.value)
+    }
+
+}
